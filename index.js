e6bc1f6c4607212ffeb282bb4f3f72d5f0e3e6a6
ELIGIO <eligioestevez@hotmail.com>
2025-09-04 21:59:57 -0400

    instalando mongoose

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! login');
+  console.log('Hello World! login');
+    res.send({mensaje: 'Hello World! login'});
 });
+
+module.exports = app;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-}); 
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+user: eligiomajestic_db_user
+pass: GkUWN3Zgcfx78gI8
