72472bfd847636636fc26a3465e908dd4e5e2520
ELIGIO <eligioestevez@hotmail.com>
2025-09-05 16:55:59 -0400

    probando bd

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.URI;
+const MONGODB_URI = process.env.URI || "mongodb+srv://Jaxsnier:Eep2265244@logincluster.oz23f9k.mongodb.net/?retryWrites=true&w=majority&appName=LoginCluster";
 const express = require('express');
 const app = express();
 
 
+mongoose.connect(MONGODB_URI);
 
-const PORT = process.env.PORT || 3000;
+const User = mongoose.model('User', new mongoose.Schema({
+  name: String,
+}));
+
+User.create(
+  { 
+    name: 'Carlos' 
+  })
+  .then(() => console.log('User created') );
 
 app.get('/', (req, res) => {
-    res.send({mensaje: 'Hello World! login'});
+    /*res.send({mensaje: 'Hello World! login'});*/
+
+    User.find().then(x=> res.send(x));
 });
 
 module.exports = app;
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`\n Server is running`);
 });
