b182758a6f4757994e3dc371aeb50166ff8a81e3
ELIGIO <eligioestevez@hotmail.com>
2025-09-08 01:19:40 -0400

    agregando cors para aceder desde local a server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const routes = express.Router();
 const auth = require('./routes/auth');
